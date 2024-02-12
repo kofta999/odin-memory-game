ed82514febcfb34f0af52b92fe44541e127a4e1a
kofta999 <mostafaxxx555@gmail.com>
2024-02-12 17:15:51 +0200

    Update CardList and util.ts

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -49,7 +49,7 @@ export default function CardList({ initialCards, setScores }: CardListProps) {
   };
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fit,16rem)] gap-16 p-10">
+    <div className="grid grid-cols-[repeat(auto-fit,16rem)] gap-16 p-10 justify-center items-center">
       {cards.map((card) => (
         <Card
           key={card._id}
